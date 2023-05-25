a903a07fd6e5addf858a1387762da41b9d3d7d99
AniketUdhane <96518558+AniketUdhane@users.noreply.github.com>
2023-05-25 17:50:29 +0530

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,3 +1,4 @@
+'''
 def job_scheduling(jobs):
     # Sort the jobs in increasing order of deadlines
     sorted_jobs = sorted(jobs, key=lambda x: x[1])
@@ -25,3 +26,26 @@ schedule = job_scheduling(jobs)
 print("Scheduled Jobs:")
 for job in schedule:
     print(f"Duration: {job[0]}, Deadline: {job[1]}")
+
+'''
+
+# User input for number of jobs and their respective deadlines and profits
+n = int(input("Enter the number of jobs: "))
+jobs = []
+for i in range(n):
+    deadline, profit = map(int, input(f"Enter deadline and profit for job {i+1}: ").split())
+    jobs.append((deadline, profit))
+
+# Sorting jobs in descending order of profit
+jobs.sort(key=lambda x: x[1], reverse=True)
+
+# Greedy algorithm for scheduling jobs
+schedule = [0] * n
+for i in range(n):
+    for j in range(min(n, jobs[i][0])-1, -1, -1):
+        if schedule[j] == 0:
+            schedule[j] = i+1
+            break
+
+# Printing the schedule
+print("Job sequence:", schedule)

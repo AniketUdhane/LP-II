b1952046be792cd8a16c71b37071e6ca27179c98
AniketUdhane <96518558+AniketUdhane@users.noreply.github.com>
2023-05-26 06:32:05 +0530

    Update 9.py

--- 9.py
+++ 9.py
@@ -1,66 +1,43 @@
-import sys
+import heapq
 
-class Graph:
-    def __init__(self, vertices):
-        self.V = vertices
-        self.graph = [[0] * vertices for _ in range(vertices)]
+def dijkstra(graph, start):
+    distances = {node: float('inf') for node in graph}
+    distances[start] = 0
+    queue = [(0, start)]
+    visited = set()
 
-    def add_edge(self, src, dest, weight):
-        self.graph[src][dest] = weight
-        self.graph[dest][src] = weight
+    while queue:
+        (cost, node) = heapq.heappop(queue)
+        if node in visited:
+            continue
+        visited.add(node)
 
-    def dijkstra(self, src):
-        dist = [sys.maxsize] * self.V  # Initialize distances to infinity
-        dist[src] = 0  # Distance from source to itself is 0
-        visited = [False] * self.V  # Track visited vertices
+        for neighbor, weight in graph[node].items():
+            if neighbor not in visited:
+                new_cost = distances[node] + weight
+                if new_cost < distances[neighbor]:
+                    distances[neighbor] = new_cost
+                    heapq.heappush(queue, (new_cost, neighbor))
 
-        for _ in range(self.V):
-            # Find the vertex with the minimum distance value
-            min_dist = sys.maxsize
-            min_index = -1
-            for v in range(self.V):
-                if not visited[v] and dist[v] < min_dist:
-                    min_dist = dist[v]
-                    min_index = v
-
-            # Mark the selected vertex as visited
-            visited[min_index] = True
-
-            # Update distance values of the adjacent vertices
-            for v in range(self.V):
-                if (
-                    not visited[v]
-                    and self.graph[min_index][v] != 0
-                    and dist[min_index] + self.graph[min_index][v] < dist[v]
-                ):
-                    dist[v] = dist[min_index] + self.graph[min_index][v]
-
-        return dist
-
-    def print_shortest_paths(self, src, dist):
-        print("Shortest paths from source vertex", src)
-        print("Vertex\tDistance")
-        for v in range(self.V):
-            print(v, "\t", dist[v])
-
+    return distances
 
 # Example usage
-g = Graph(9)
-g.add_edge(0, 1, 4)
-g.add_edge(0, 7, 8)
-g.add_edge(1, 2, 8)
-g.add_edge(1, 7, 11)
-g.add_edge(2, 3, 7)
-g.add_edge(2, 8, 2)
-g.add_edge(2, 5, 4)
-g.add_edge(3, 4, 9)
-g.add_edge(3, 5, 14)
-g.add_edge(4, 5, 10)
-g.add_edge(5, 6, 2)
-g.add_edge(6, 7, 1)
-g.add_edge(6, 8, 6)
-g.add_edge(7, 8, 7)
+graph = {}
 
-src = 0  # Source vertex
-dist = g.dijkstra(src)
-g.print_shortest_paths(src, dist)
+n = int(input("Enter number of nodes: "))
+
+for i in range(n):
+    node = input(f"Enter name of node {i+1}: ")
+    edges = {}
+    while True:
+        neighbor = input(f"Enter neighbor of {node} (or type 'done' to finish): ")
+        if neighbor == 'done':
+            break
+        weight = int(input(f"Enter weight of edge between {node} and {neighbor}: "))
+        edges[neighbor] = weight
+    graph[node] = edges
+
+start = input("Enter starting node: ")
+distances = dijkstra(graph, start)
+
+print(distances)

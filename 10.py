06573676d5ccee20ceac1c02b3363f7041ba26c1
AniketUdhane <96518558+AniketUdhane@users.noreply.github.com>
2023-05-26 08:48:51 +0530

    Update 10.py

--- 10.py
+++ 10.py
@@ -48,3 +48,51 @@ n = int(input("Enter the size of the chessboard: "))
 queens = nQueens(n)
 queens.solve(0)
 queens.print_solutions()
+
+'''
+
+Objective:
+ Understand and implement Constraint Satisfaction Problem using Branch and Bound for nqueens problem
+ Understand and implement Constraint Satisfaction Problem using Backtracking for n-queens
+problem
+
+Outcome:
+ Ability to choose an appropriate problem solving method and knowledge representation
+technique
+
+Software Required:
+ Python
+
+Theory:
+
+ N Queen Problem using Backtracking:
+The N Queen is the problem of placing N chess queens on an N×N chessboard so that no two
+queens attack each other. For example, the following is a solution for the 4 Queen problem.
+
+The expected output is a binary matrix that has 1s for the blocks where queens are placed. For
+example, the following is the output matrix for the above 4 queen solution.
+
+ Backtracking Algorithm:
+The idea is to place queens one by one in different columns, starting from the leftmost
+column. When we place a queen in a column, we check for clashes with already placed
+queens. In the current column, if we find a row for which there is no clash, we mark this
+row and column as part of the solution. If we do not find such a row due to clashes, then
+we backtrack and return false.
+1. Start in the leftmost column
+2. If all queens are placed
+ return true
+3. Try all rows in the current column.
+ Do following for every tried row.
+a) If the queen can be placed safely in this row then mark this [row, column] as
+part of the solution and recursively check if placing queen here leads to a
+solution.
+b) If placing the queen in [row, column] leads to a solution then return true.
+c) If placing queen doesn't lead to a solution then unmark this [row, column]
+(Backtrack) and go to step (a) to try other rows.
+4. If all rows have been tried and nothing worked, return false to trigger
+backtracking.
+
+Conclusion: Thus we have Implement a solution for a Constraint Satisfaction Problem using Branch
+and Bound and Backtracking for n-queens problem.
+
+'''
